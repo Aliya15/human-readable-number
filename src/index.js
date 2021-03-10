a7abd264e0354cdef24ef2b4fbff76b613815f83
Aliya <aliyuwe4ka@gmail.com>
2021-03-10 19:15:07 +0300

    feat: result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tenth = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let numberString = number.toString();
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (number < 20) {
+        return ones[number].trim();
+    }
+
+    if (numberString.length == 2) {
+        return (tenth[numberString[0]] + ' ' + ones[numberString[1]]).trim();
+    }
+
+    if (numberString.length == 3) {
+        if (numberString[1] === '0' && numberString[2] === '0') {
+            return (ones[numberString[0]] + ' hundred').trim();
+        }
+        else {
+            let result = ones[numberString[0]] + ' hundred ' + toReadable(+(numberString[1] + numberString[2]));
+            return result.trim();
+        }
+
+    }
 }
